cd7d3b209c159b40e233881a78218f9c6864a1b9
Mohammed Nady <muhammednadyii@gmail.com>
2026-07-13 10:49:06 +0300

    Removed old Project

--- FreelanceApplication/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FreelanceApplication.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
