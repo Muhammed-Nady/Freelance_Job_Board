747e8f7bd0c4349da6f4857e077d9a83febe3390
Mohamed Magdy <ymmagdyfofo@gmail.com>
2026-07-13 18:24:08 +0300

    Add Client and Freelancer profile entities with migration

---
+++ Freelify/Models/Entities/FreelancerProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace Freelify.Models.Entities
+{
+    public class FreelancerProfile : ApplicationUser
+    {
+        public string Bio { get; set; }
+
+        public string Experience { get; set; }
+
+
+        // TODO
+        //reviews
+        //avgrating
+        //reviewCount
+    }
+}
