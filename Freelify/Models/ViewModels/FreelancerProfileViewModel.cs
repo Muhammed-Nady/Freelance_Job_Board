a854e5197c7f4e82deb0f75a3b20ed735e0fe289
Mohamed Magdy <ymmagdyfofo@gmail.com>
2026-07-16 02:57:29 +0300

    Add client and freelancer profile view models.

---
+++ Freelify/Models/ViewModels/FreelancerProfileViewModel.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Freelify.Models.ViewModels
+{
+    public class FreelancerProfileViewModel
+    {
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? ProfileImageUrl { get; set; }
+
+        public string Bio { get; set; }
+
+        public string Experience { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
